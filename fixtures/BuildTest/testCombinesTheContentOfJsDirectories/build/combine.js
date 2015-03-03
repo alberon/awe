ec361d15df8a001247fd59abafb51559221d3917
Dave James Miller <dave@davejamesmiller.com>
2015-03-03 22:11:42 +0000

    Remove build files from Git

--- fixtures/BuildTest/testCombinesTheContentOfJsDirectories/build/combine.js
+++
@@ -1,5 +0,0 @@
-f1();
-(function() {
-  f2();
-
-}).call(this);
--- fixtures/BuildTest/testCreatesSourceMapsForCombinedJavascriptDirectories/build/combine.js
+++
@@ -1,8 +0,0 @@
-// This is just to move it down a line
-console.log('JavaScript');
-(function() {
-  console.log('CoffeeScript');
-
-}).call(this);
-
-//# sourceMappingURL=combine.js.map
